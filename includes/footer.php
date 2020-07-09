02934dee9185798978fd293e12afd09dab4d85c3
batsie <46093695+Batsiraik@users.noreply.github.com>
2020-07-09 18:18:06 +0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer-area section-gap">
+        <div class="container">
+        
+            <div class="footer-bottom row align-items-center text-center text-lg-left">
+                <p class="footer-text m-0 col-lg-8 col-md-12"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | <i class="ti-heart" aria-hidden="true"></i> by <a href="" target="_blank">BatsieBrilliant</a>
+</p>
+            </div>
+        </div>
+    </footer>
